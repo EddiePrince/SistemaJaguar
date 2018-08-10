9ab6806eab9ea21b53b3760bf49a4a0c3a809ffd
EddiePrince <36989188+EddiePrince@users.noreply.github.com>
2018-08-10 10:22:25 -0300

    Cadastro de Unidades

--- CadastroUnidade.php
+++ CadastroUnidade.php
@@ -37,19 +37,114 @@
 				<form method="post" action="post_cadastro_unidades.php"><br>
 
 					<label>Nome Da Unidade:</label><br>
-					<input class="form-control" type="text" name="nome" required autofocus><br>
+					<input class="form-control" type="text" name="nomeUnidade"  autofocus><br>
 
 					<label>CNPJ:</label><br>
-					<input class="form-control" type="text" name="cnpj" required><br>
+					<input class="form-control" type="text" name="cnpj" ><br>
+
 
 					<label>Tipo da Unidade:</label><br>
 					<select class="form-control" name="tipoUnidade">
-          <option>Selecione o Tipo da Unidade</option>
-          <option>Central</option>
-          <option>Regional</option>
+          				<option >Selecione o Tipo da Unidade</option>
+          				<option>Central</option>
+         		 		<option>Regional</option>
         </select><br><br>
 
-					<button class="btn btn-default" type="submit">Cadastrar</button>
+        		<p>Unidade Federal:</p>
+						<select class="form-control" name="uf">
+							<option value="Estado Nao Selecionado">Selecione o Estado</option>
+									<option value="AC">Acre</option>
+									<option value="AL">Alagoas</option>
+									<option value="AM">Amazonas</option>
+									<option value="AP">Amapá</option>
+									<option value="BA">Bahia</option>
+									<option value="CE">Ceará</option>
+									<option value="DF">Distrito Federal</option>
+									<option value="ES">Espírito Santo</option>
+									<option value="GO">Goiás</option>
+									<option value="MA">Maranhão</option>
+									<option value="MT">Mato Grosso</option>
+									<option value="MS">Mato Grosso do Sul</option>
+									<option value="MG">Minas Gerais</option>
+									<option value="PA">Pará</option>
+									<option value="PB">Paraíba</option>
+									<option value="PR">Paraná</option>
+									<option value="PE">Pernambuco</option>
+									<option value="PI">Piauí</option>
+									<option value="RJ">Rio de Janeiro</option>
+									<option value="RN">Rio Grande do Norte</option>
+									<option value="RO">Rondônia</option>
+									<option value="RS">Rio Grande do Sul</option>
+									<option value="RR">Roraima</option>
+									<option value="SC">Santa Catarina</option>
+									<option value="SE">Sergipe</option>
+									<option value="SP">São Paulo</option>
+									<option value="TO">Tocantins</option>
+							</select>
+
+						<p>Municipio:</p>
+						<select class="form-control" name="municipio">
+						<option value="municipio Nao Selecionado">Selecione o Municipio</option>
+						<option value="Aguiarnópolis">Aguiarnópolis</option>
+						<option value="Almas">Almas</option>
+						<option value="Ananás">Ananás</option>
+						<option value="Araguacema">Araguacema</option>
+						<option value="Araguaína">Araguaína</option>
+						<option value="Araguanã">Araguanã</option>
+						<option value="Araguatins">Araguatins</option>
+						<option value="Arraias">Arraias</option>
+						<option value="Augustinópolis">Augustinópolis</option>
+						<option value="Aurora do Tocantins">Aurora do Tocantins</option>
+						<option value="Babaçulândia">Babaçulândia</option>
+						<option value="Brejinho de Nazaré">Brejinho de Nazaré</option>
+						<option value="Caseara">Caseara</option>
+						<option value="Darcinópolis">Darcinópolis</option>
+						<option value="Dianópolis">Dianópolis</option>
+						<option value="Esperantina">Esperantina</option>
+						<option value="Filadélfia">Filadélfia</option>
+						<option value="Formoso do Araguaia">Formoso do Araguaia</option>
+						<option value="Gurupi">Gurupi</option>
+						<option value="Ipueiras">Ipueiras</option>
+						<option value="Itaguatins">Itaguatins</option>
+						<option value="Lagoa da Confusão">Lagoa da Confusão</option>
+						<option value="Lajeado">Lajeado</option>
+						<option value="Mateiros">Mateiros</option>
+						<option value="Monte do Carmo">Monte do Carmo</option>
+						<option value="Natividade">Natividade</option>
+						<option value="Novo Acordo">Novo Acordo</option>
+						<option value="Palmas">Palmas</option>
+						<option value="Paraíso do Tocantins">Paraíso do Tocantins</option>
+						<option value="Paranã">Paranã</option>
+						<option value="Pau d'Arco">Pau d'Arco</option>
+						<option value="Peixe">Peixe</option>
+						<option value="Pium">Pium</option>
+						<option value="Ponte Alta do Tocantins">Ponte Alta do Tocantins</option>
+						<option value="Porto Nacional">Porto Nacional</option>
+						<option value="Praia Norte">Praia Norte</option>
+						<option value="Rio da Conceição">Rio da Conceição</option>
+						<option value="Sampaio">Sampaio</option>
+						<option value="São Bento do Tocantins">São Bento do Tocantins</option>
+						<option value="São Félix do Tocantins">São Félix do Tocantins</option>
+						<option value="São Sebastião do Tocantins">São Sebastião do Tocantins</option>
+						<option value="Taguatinga">Taguatinga</option>
+						<option value="Tocantinópolis">Tocantinópolis</option>
+						<option value="Wanderlândia">Wanderlândia</option>
+						<option value="Xambioá">Xambioá</option>
+					</select><br>
+
+						<p>CEP:</p>
+						<p><input class="form-control" type="text" name="cep"></p>
+
+						<p>Bairro:</p>
+						<p><input class="form-control" type="text" name="bairro"></p>
+
+						<p>Lote:</p>
+						<p><input class="form-control" type="text" name="lote"></p>
+
+						<p>Complemento:</p>
+						<p><input class="form-control" type="text" name="complemento"></p>
+
+					<input class="btn btn-default" name="submit" type="submit" value="Salvar">
 				</form>
 			</div>
 		</div>
