5863a43d8a4f6bae9040478a4d07bf633ec0876f
EddiePrince <36989188+EddiePrince@users.noreply.github.com>
2018-08-10 11:55:22 -0300

    Listagem de animais
    Incluir link para perfil de cada um

--- listaAnimais.inc.php
+++ listaAnimais.inc.php
@@ -1,8 +1,3 @@
-<?php
-//Conecção com Banco
-  include 'conexao.inc.php';//inclusão do arquivo de conexão com o banco
-  include 'verificausuario.inc.php';
-?>
 
 <?php
 	$sql = "SELECT * FROM animais";
@@ -11,60 +6,15 @@
 	if($result->num_rows > 0){
 		while($row = $result->fetch_assoc())
 		{
-			$con1;
-			if ($row['condicao1'] == 1) {
-				$con1='Atendida';
-			}
-			else{
-				$con1 = "Não Atendida";
-			};
+			
 
-			$con2;
-			if ($row['condicao2'] == 1) {
-				$con2='Atendida';
-			}
-			else{
-				$con2 = "Não Atendida";
-			};
-
-			$con3;
-			if ($row['condicao3'] == 1) {
-				$con3='Atendida';
-			}
-			else{
-				$con3 = "Não Atendida";
-			};
 			echo "<tr>";
-			echo "<td>" . $row['nFicha'] . "</td>";
-			echo "<td>" . $row['dataRecebimento'] . "</td>";
-			echo "<td>" . $row['agente'] . "</td>";
-			echo "<td>" . $row['tipoRecolhimento'] . "</td>";
-			echo "<td>" . $row['nomeEntregador'] . "</td>";
-			echo "<td>" . $row['cpf_cnpj'] . "</td>";
-			echo "<td>" . $row['telefone'] . "</td>";
-			echo "<td>" . $row['endereco'] . "</td>";
-			echo "<td>" . $row['municipio'] . "</td>";
-			echo "<td>" . $row['cep'] . "</td>";
-			echo "<td>" . $row['idDocumento'] . "</td>";
-			echo "<td>" . $row['idUnidade'] . "</td>";
-			echo "<td>" . $row['municipioProcedencia'] . "</td>";
-			echo "<td>" . $row['uf'] . "</td>";
-			echo "<td>" . $row['ultimaProcedencia'] . "</td>";
-			echo "<td>" . $row['dieta'] . "</td>";
+			echo "<td>" . $row['idAnimal'] . "</td>";
 			echo "<td>" . $row['nomeComum'] . "</td>";
-			echo "<td>" . $row['nomeCientifico'] . "</td>";
-			echo "<td>" . $row['familia'] . "</td>";
-			echo "<td>" . $row['ordem'] . "</td>";
-			echo "<td>" . $row['codMarca'] . "</td>";
-			echo "<td>" . $row['tipoMarca'] . "</td>";
-			echo "<td>" . $row['situacao'] . "</td>";
-			echo "<td>" . $row['periodoQuarentena'] . "</td>";
-			echo "<td>" . $row['tipoDestinacao'] . "</td>";
-			echo "<td>" . $row['subtipoDestinacao'] . "</td>";
-			echo "<td>" . $con1 . "</td>";
-			echo "<td>" . $con2 . "</td>";
-			echo "<td>" . $con3 . "</td>";
-			echo "<td>" . $row['idArea'] . "</td>";
+			echo "<td>" . $row['data'] . "</td>";
+			echo "<td>" . $row['nomeAgente'] . "</td>";
+			echo "<td>" . $row['nomeEntregador'] . "</td>";
+			
 			echo "</tr>";
 		}
 	}
@@ -72,4 +22,4 @@
 			echo "Dados não encontrados";
 	}
 
-	?>
+?>
--- listaAnimais.php
+++ listaAnimais.php
@@ -1,7 +1,7 @@
 <?php
   //Conexão com Banco
   include 'conexao.inc.php';//inclusão do arquivo de conexão com o banco
-  //include '../verificausuario.inc.php';
+  //include 'verificausuario.inc.php';
 ?>
 
 <!DOCTYPE html>
@@ -10,8 +10,8 @@
 <head>
   <meta charset="utf8mb4_unicode_ci">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="../style.css">
-	<link rel="stylesheet" type="text/css" href="../style/bootstrapv3.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="style/bootstrapv3.min.css">
 	<title>Cadastro de Unidades</title>
 </head>
 
@@ -20,16 +20,16 @@
 		<div class="container-fluid">
 
 			<dir class="logo_admin">
-				 <img src="../img/logo.png">
+				 <img src="img/logo.png">
 			</dir>
 			<dir class="logout">
-				<a href="../areaadm.php"> <input class="btn btn-default" type="button" name="btn" value="Voltar"></a>
-				<a  class="btn btn-default" href="../logout.php">Logout</a>
+				<a href="areaadm.php"> <input class="btn btn-default" type="button" name="btn" value="Voltar"></a>
+				<a  class="btn btn-default" href="logout.php">Logout</a>
 			</dir>
 			<div >
 				<div class="">
 				<!-- Menu -->
-				<?php include '../menu/menuAdm.php'; ?>
+				<?php include 'menu/menuAdm.php'; ?>
 				<!-- Fim Menu -->
 			</div><br><br>
 
@@ -38,10 +38,13 @@
 		<h2>Lista de Animais</h2><br>
   	<table border="1px">
   		<tr>
-  			<th><a href="perfilAnimal.php">Ver Perfil</a><br>Animal X</th>
-  			<th><a href="perfilAnimal.php">Ver Perfil</a><br>Animal Y</th>
-  			<th><a href="perfilAnimal.php">Ver Perfil</a><br>Animal Z</th>
+  			<th>Número da Ficha</th>
+  			<th>Nome Comum</th>
+  			<th>Data de Recebimento</th>
+  			<th>Agente</th>
+  			<th>Perfil</th>
         </tr>
+      <?php include 'listaAnimais.inc.php' ?>
 
   	</table>
   </div>
