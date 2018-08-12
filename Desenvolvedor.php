e7a1b45b064d530a050c78d025173e8c17a48cbf
tecnicowilliam <williamtec.inf.int@gmail.com>
2018-08-12 10:49:14 -0300

    Área Desenvolvedor

---
+++ Desenvolvedor.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="br-pt">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="style/bootstrapv3.min.css">
+		<title>Recuperação de Senha</title>
+	</head>
+	<body>
+
+		<div id="page">
+			<div class="container-fluid">
+
+				<div class="logo_admin">
+					<img src="img/logo.png" alt="Logo">
+				</div>
+
+				<div class="container">
+					<div class="corpoAdm" id="corpoAdm">
+							<h2>Recuperação de Senha</h2>
+
+							<table class="table">
+
+							</table>
+
+					</div>
+					<div class="footer">
+						<?php include 'footer.php'; ?>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</body>
+</html>
