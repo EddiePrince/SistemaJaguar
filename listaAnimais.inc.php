02592195beea7a29e8d257566337495fd7a6a405
EddiePrince <36989188+EddiePrince@users.noreply.github.com>
2018-08-13 10:17:28 -0300

    Lista de Animais

--- listaAnimais.inc.php
+++ listaAnimais.inc.php
@@ -6,15 +6,12 @@
 	if($result->num_rows > 0){
 		while($row = $result->fetch_assoc())
 		{
-			
-
 			echo "<tr>";
 			echo "<td>" . $row['idAnimal'] . "</td>";
 			echo "<td>" . $row['nomeComum'] . "</td>";
 			echo "<td>" . $row['data'] . "</td>";
 			echo "<td>" . $row['nomeAgente'] . "</td>";
-			echo "<td>" . $row['nomeEntregador'] . "</td>";
-			
+			echo "<td><a href='perfilAnimal.php?id=" . $row['idAnimal'] . "/'>" ."<img class='linkicon' src='img/linkicon.png'></a></td>";
 			echo "</tr>";
 		}
 	}
