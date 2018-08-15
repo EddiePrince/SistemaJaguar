0ee04aa6715fc78d65a2140a4429aec8ccd8311e
R0mul0 <31490516+R0mul0@users.noreply.github.com>
2018-08-15 11:44:02 -0300

    Tela Cadastrar de unidades Administrador

--- CadastroUnidade.php
+++ CadastroUnidade.php
@@ -30,23 +30,23 @@
 					<!-- Fim Menu -->
 
 				<form method="post" action="post_cadastro_unidades.php"><br>
-          <h3>Cadastro de Unidades</h3><br>
+          			<h3>Cadastro de Unidades</h3><br>
 
-					<label>Nome Da Unidade:</label><br>
-					<input class="form-control" type="text" name="nomeUnidade"  autofocus><br>
+					<label>Nome Da Unidade:</label>
+					<input class="form-control" type="text" name="nomeUnidade"  autofocus>
 
-					<label>CNPJ:</label><br>
-					<input class="form-control" type="text" name="cnpj" ><br>
+					<label>CNPJ:</label>
+					<input class="form-control" type="text" name="cnpj">
 
 
 					<label>Tipo da Unidade:</label><br>
 					<select class="form-control" name="tipoUnidade">
           				<option >Selecione o Tipo da Unidade</option>
-          				<option>Central</option>
+          					<option>Central</option>
          		 		<option>Regional</option>
-        </select><br><br>
+        			</select><br>
 
-        		<p>Unidade Federal:</p>
+        			<p>Unidade Federal:</p>
 						<select class="form-control" name="uf">
 							<option value="Estado Nao Selecionado">Selecione o Estado</option>
 									<option value="AC">Acre</option>
@@ -76,71 +76,73 @@
 									<option value="SE">Sergipe</option>
 									<option value="SP">São Paulo</option>
 									<option value="TO">Tocantins</option>
-							</select>
+						</select>
 
 						<p>Municipio:</p>
-					<select class="form-control" name="municipio">
-						<option value="municipio Nao Selecionado">Selecione o Municipio</option>
-						<option value="Aguiarnópolis">Aguiarnópolis</option>
-						<option value="Almas">Almas</option>
-						<option value="Ananás">Ananás</option>
-						<option value="Araguacema">Araguacema</option>
-						<option value="Araguaína">Araguaína</option>
-						<option value="Araguanã">Araguanã</option>
-						<option value="Araguatins">Araguatins</option>
-						<option value="Arraias">Arraias</option>
-						<option value="Augustinópolis">Augustinópolis</option>
-						<option value="Aurora do Tocantins">Aurora do Tocantins</option>
-						<option value="Babaçulândia">Babaçulândia</option>
-						<option value="Brejinho de Nazaré">Brejinho de Nazaré</option>
-						<option value="Caseara">Caseara</option>
-						<option value="Darcinópolis">Darcinópolis</option>
-						<option value="Dianópolis">Dianópolis</option>
-						<option value="Esperantina">Esperantina</option>
-						<option value="Filadélfia">Filadélfia</option>
-						<option value="Formoso do Araguaia">Formoso do Araguaia</option>
-						<option value="Gurupi">Gurupi</option>
-						<option value="Ipueiras">Ipueiras</option>
-						<option value="Itaguatins">Itaguatins</option>
-						<option value="Lagoa da Confusão">Lagoa da Confusão</option>
-						<option value="Lajeado">Lajeado</option>
-						<option value="Mateiros">Mateiros</option>
-						<option value="Monte do Carmo">Monte do Carmo</option>
-						<option value="Natividade">Natividade</option>
-						<option value="Novo Acordo">Novo Acordo</option>
-						<option value="Palmas">Palmas</option>
-						<option value="Paraíso do Tocantins">Paraíso do Tocantins</option>
-						<option value="Paranã">Paranã</option>
-						<option value="Pau d'Arco">Pau d'Arco</option>
-						<option value="Peixe">Peixe</option>
-						<option value="Pium">Pium</option>
-						<option value="Ponte Alta do Tocantins">Ponte Alta do Tocantins</option>
-						<option value="Porto Nacional">Porto Nacional</option>
-						<option value="Praia Norte">Praia Norte</option>
-						<option value="Rio da Conceição">Rio da Conceição</option>
-						<option value="Sampaio">Sampaio</option>
-						<option value="São Bento do Tocantins">São Bento do Tocantins</option>
-						<option value="São Félix do Tocantins">São Félix do Tocantins</option>
-						<option value="São Sebastião do Tocantins">São Sebastião do Tocantins</option>
-						<option value="Taguatinga">Taguatinga</option>
-						<option value="Tocantinópolis">Tocantinópolis</option>
-						<option value="Wanderlândia">Wanderlândia</option>
-						<option value="Xambioá">Xambioá</option>
-					</select><br>
+							<select class="form-control" name="municipio">
+								<option value="municipio Nao Selecionado">Selecione o Municipio</option>
+								<option value="Aguiarnópolis">Aguiarnópolis</option>
+								<option value="Almas">Almas</option>
+								<option value="Ananás">Ananás</option>
+								<option value="Araguacema">Araguacema</option>
+								<option value="Araguaína">Araguaína</option>
+								<option value="Araguanã">Araguanã</option>
+								<option value="Araguatins">Araguatins</option>
+								<option value="Arraias">Arraias</option>
+								<option value="Augustinópolis">Augustinópolis</option>
+								<option value="Aurora do Tocantins">Aurora do Tocantins</option>
+								<option value="Babaçulândia">Babaçulândia</option>
+								<option value="Brejinho de Nazaré">Brejinho de Nazaré</option>
+								<option value="Caseara">Caseara</option>
+								<option value="Darcinópolis">Darcinópolis</option>
+								<option value="Dianópolis">Dianópolis</option>
+								<option value="Esperantina">Esperantina</option>
+								<option value="Filadélfia">Filadélfia</option>
+								<option value="Formoso do Araguaia">Formoso do Araguaia</option>
+								<option value="Gurupi">Gurupi</option>
+								<option value="Ipueiras">Ipueiras</option>
+								<option value="Itaguatins">Itaguatins</option>
+								<option value="Lagoa da Confusão">Lagoa da Confusão</option>
+								<option value="Lajeado">Lajeado</option>
+								<option value="Mateiros">Mateiros</option>
+								<option value="Monte do Carmo">Monte do Carmo</option>
+								<option value="Natividade">Natividade</option>
+								<option value="Novo Acordo">Novo Acordo</option>
+								<option value="Palmas">Palmas</option>
+								<option value="Paraíso do Tocantins">Paraíso do Tocantins</option>
+								<option value="Paranã">Paranã</option>
+								<option value="Pau d'Arco">Pau d'Arco</option>
+								<option value="Peixe">Peixe</option>
+								<option value="Pium">Pium</option>
+								<option value="Ponte Alta do Tocantins">Ponte Alta do Tocantins</option>
+								<option value="Porto Nacional">Porto Nacional</option>
+								<option value="Praia Norte">Praia Norte</option>
+								<option value="Rio da Conceição">Rio da Conceição</option>
+								<option value="Sampaio">Sampaio</option>
+								<option value="São Bento do Tocantins">São Bento do Tocantins</option>
+								<option value="São Félix do Tocantins">São Félix do Tocantins</option>
+								<option value="São Sebastião do Tocantins">São Sebastião do Tocantins</option>
+								<option value="Taguatinga">Taguatinga</option>
+								<option value="Tocantinópolis">Tocantinópolis</option>
+								<option value="Wanderlândia">Wanderlândia</option>
+								<option value="Xambioá">Xambioá</option>
+							</select><br>
 
+						<div class="endereco">
 						<p>CEP:</p>
-						<p><input class="form-control" type="text" name="cep"></p>
+						<input class="form-control" type="text1" name="cep">
 
 						<p>Bairro:</p>
-						<p><input class="form-control" type="text" name="bairro"></p>
+						<input class="form-control" type="text1" name="bairro">
 
 						<p>Lote:</p>
-						<p><input class="form-control" type="text" name="lote"></p>
+						<input class="form-control" type="text1" name="lote">
 
 						<p>Complemento:</p>
-						<p><input class="form-control" type="text" name="complemento"></p>
+						<input class="form-control" type="text1" name="complemento">
+						</div>
 
-					<input class="btn btn-default" name="submit" type="submit" value="Salvar">
+					<input class="btn btn-default" id="salva" name="submit" type="submit" value="Salvar">
 				</form>
 			</div>
       </div>
