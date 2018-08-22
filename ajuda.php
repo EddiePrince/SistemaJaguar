8b6e841a54e7a2c9c7b5425c69eb0e560c1eb1d0
tecnicowilliam <40693817+tecnicowilliam@users.noreply.github.com>
2018-08-22 11:52:41 -0300

    estilizando ajuda

--- ajuda.php
+++ ajuda.php
@@ -21,13 +21,13 @@
 					<div  class="corpoAdm" id="corpoAdm">
 
 
-					<div class="container">
+				<div class="container">
 
 				<div class="container theme-showcase" role="main">
 
 				<div class="page-header">
 					<h3>Instruções Básicas do Sistema</h3><br><br>
-				</div>
+
 
 				<div>
 					<!-- Abas de Navegação -->
@@ -42,68 +42,68 @@
 
 					<!-- Conteúdo refente a cada aba de Navegação -->
 				<div class="tab-content">
-						<div role="tabpanel" class="tab-pane active" id="home">
+						<div role="tabpanel" class="tab-pane active" id="home"><br>
 							<p>
-								Sobre o Sistema: É um software de gestão de animais silvestre. Tem a finalidade de auxiliar o gerenciamento integrado e centralizado das informações. Utiliza algumas tecnologias livres e conhecidas. Pode ser utilizado em ambiente Windows ou Linux, pois
+								<h3> <strong class="titulo">Sobre o Sistema: </strong></h3> É um software de gestão de animais silvestre. Tem a finalidade de auxiliar o gerenciamento integrado e centralizado das informações. Utiliza algumas tecnologias livres e conhecidas. Pode ser utilizado em ambiente Windows ou Linux, pois
 								necessita apenas de um navegador Web e um usuário e senha de acesso.
 							</p>
 						</div>
-						<div role="tabpanel" class="tab-pane" id="profile">
+						<div role="tabpanel" class="tab-pane" id="profile"><br>
 							<p>
-								Conhecendo o Sistema: Nessa seção, iremos apresentar como acessar o sistema e como funcionam os botões, listas, filtros de busca e seleção das páginas do sistema. Para acessar o sistema basta abrir o navegador, acessar o endereço na internet onde o sistema
+								<h3><strong class="titulo">Conhecendo o Sistema: </strong></h3>  Nessa seção, iremos apresentar como acessar o sistema e como funcionam os botões, listas, filtros de busca e seleção das páginas do sistema. Para acessar o sistema basta abrir o navegador, acessar o endereço na internet onde o sistema
 								foi disponibilizado e então informar seu login e senha, fornecida pelo administrador do sistema. Existe também uma opção para recuperar senha, que enviará um e-mail com instruções para recuperá-la. Depois de efetuar o login serão exibidos o módulo
 								do sistema. Quando um cadastro é aberto uma listagem de registro é exibida, e ao clicar em um destes registros é aberta uma nova página com opções específicas para esse registro.
 							</p>
 						</div>
-						<div role="tabpanel" class="tab-pane" id="messages">
+						<div role="tabpanel" class="tab-pane" id="messages"><br>
 							<p>
-								Filtro de Busca: Quando os cadastros são abertos, serão exibidos campos que podem ser usados para filtrar os registros da listagem. Com os filtros de busca é possível, por exemplo, buscar todos os animais silvestres com uma data de entrada no sistema.
+							 <h3><strong class="titulo">Filtro de Busca: </strong></h3>	 Quando os cadastros são abertos, serão exibidos campos que podem ser usados para filtrar os registros da listagem. Com os filtros de busca é possível, por exemplo, buscar todos os animais silvestres com uma data de entrada no sistema.
 								Um componente que os filtros de busca podem exibir é a busca rápida. Nessa etapa o usuário pode digitar o nome, ou outra referência que esteja descrita no campo e o mesmo irá mostrar uma listagem que contém as informações que foram digitas.
 							</p>
 						</div>
-						<div role="tabpanel" class="tab-pane" id="settings">
+						<div role="tabpanel" class="tab-pane" id="settings"><br>
 							<p>
-								Permissões: Nessa seção iremos apresentar como são configurados os usuários que podem acessar o sistema e como ser atribuídas suas permissões, bloqueios e demais configurações de segurança.
+								<h3><strong class="titulo">Permissões: </strong></h3> Nessa seção iremos apresentar como são configurados os usuários que podem acessar o sistema e como ser atribuídas suas permissões, bloqueios e demais configurações de segurança.
 							</p>
 						</div>
-						<div role="tabpanel" class="tab-pane" id="detalhes">
+						<div role="tabpanel" class="tab-pane" id="detalhes"><br>
 							<p>
-								Usuários e Tipos de Usuários: O cadastro de tipo de usuário define as categorias de usuários existentes atualmente, as quais garantem um nível hierárquico de acesso a determinadas funcionalidade do sistema. O cadastro de usuários, propriamente dito, permite
+								<h3><strong class="titulo">Usuários e Tipos de Usuários: </strong></h3> O cadastro de tipo de usuário define as categorias de usuários existentes atualmente, as quais garantem um nível hierárquico de acesso a determinadas funcionalidade do sistema. O cadastro de usuários, propriamente dito, permite
 								o cadastro dos usuários que irão acessar o sistema, respeitando o seu tipo e as demais atribuições de acesso para a instituição, NATURATINS.
 							</p>
-							Cadastro do Sistema: Nesta seção iremos apresentar o módulo de endereçamento pessoas físicas e jurídicas. Módulo Endereçamento: Neste módulo devem ser inseridos os atributos das U.R (unidade Regionais, NATURATINS). Módulo Pessoa Física/Jurídica: Neste
+						 <h3><strong class="titulo">Cadastro do Sistema: </strong></h3>	Nesta seção iremos apresentar o módulo de endereçamento pessoas físicas e jurídicas. Módulo Endereçamento: Neste módulo devem ser inseridos os atributos das U.R (unidade Regionais, NATURATINS). Módulo Pessoa Física/Jurídica: Neste
 							módulo você pode inserir as informações das pessoas físicas e jurídicas que serão trabalhadas dentro do sistema. Unidade Regionais: Neste cadastro você poderá inserir as regionais responsáveis pela entrega do animal silvestre.
 
 						</div>
-						<div role="tabpanel" class="tab-pane" id="tutores">
-							<br> Glossário:
+						<div role="tabpanel" class="tab-pane" id="tutores"><br>
+							<br> <h3> <strong class="titulo">Glossário: </strong></h3>
 							<p>
-							<br>NATURATINS: Instituo Natureza do Tocantins.
+							<br><strong class="subtitulo">NATURATINS: </strong>Instituo Natureza do Tocantins.
 
-							<br>U.R: Unidade Regional.
+							<br><strong class="subtitulo">U.R: </strong>Unidade Regional.
 
-							<br>Banco de Dados: Banco de dados, (ou base de dados), são conjuntos de dados com uma estrutura regular que organizam informação. Um banco de dados normalmente agrupa informações utilizadas para o mesmo fim.
+							<br><strong class="subtitulo">Banco de Dados: </strong>Banco de dados, (ou base de dados), são conjuntos de dados com uma estrutura regular que organizam informação. Um banco de dados normalmente agrupa informações utilizadas para o mesmo fim.
 
-							<br>Dados: Os dados são qualquer registro ou indícios relacionáveis a alguma entidade ou evento. Por exemplo, um documento de identificação pode conter vários dados de uma pessoa com nome, sexo, data nascimento, etc.
+							<br><strong class="subtitulo">Dados: </strong>Os dados são qualquer registro ou indícios relacionáveis a alguma entidade ou evento. Por exemplo, um documento de identificação pode conter vários dados de uma pessoa com nome, sexo, data nascimento, etc.
 
-							<br>E-MAIL: E-mail, correio-e, ou correi eletrônico, é um método que permite compor, enviar e receber mensagens através de sistema eletrônicos de comunicação.
+							<br><strong class="subtitulo">E-MAIL: </strong>E-mail, correio-e, ou correi eletrônico, é um método que permite compor, enviar e receber mensagens através de sistema eletrônicos de comunicação.
 
-							<br>ÍCONE: Um ícone em informática é um pequeno símbolo gráfico, usado geralmente para representar um software ou um atalho para software ou pasta.
+							<br><strong class="subtitulo">ÍCONE: </strong>Um ícone em informática é um pequeno símbolo gráfico, usado geralmente para representar um software ou um atalho para software ou pasta.
 
-							<br>USUÀRIOS: São determinados grupos de pessoas que utilizam de algum tipo de serviço, nesse caso serviço de informações, eles podem ser divididos ou classificados de acordo com uma determinada área de interesse.
+							<br><strong class="subtitulo">USUÀRIOS: </strong>São determinados grupos de pessoas que utilizam de algum tipo de serviço, nesse caso serviço de informações, eles podem ser divididos ou classificados de acordo com uma determinada área de interesse.
 
-							<br>SISTEMA: É o termo utilizado para descrever um sistema automatizado (que pode ser denominado com o Sistema de Informação Computadorizado) ou manual, que abrange pessoas, máquinas, e/ou outros métodos organizados para coletar, processar, transmitir
+							<br><strong class="subtitulo">SISTEMA: </strong>É o termo utilizado para descrever um sistema automatizado (que pode ser denominado com o Sistema de Informação Computadorizado) ou manual, que abrange pessoas, máquinas, e/ou outros métodos organizados para coletar, processar, transmitir
 							e disseminar dados que representam informação para usuário.
 
-							<br>INTRANET: É uma rede de computadores privativa que utiliza as mesmas tecnologias que são utilizadas na internet.
+							<br><strong class="subtitulo">INTRANET: </strong>É uma rede de computadores privativa que utiliza as mesmas tecnologias que são utilizadas na internet.
 
-							<br>LINKS: Uma hiperligação, ou simplesmente uma ligação, é uma referência num documento hipertexto a outro documento ou a outro recurso. O seu significado é “atalho”, “caminho” ou “ligação”.
+							<br><strong class="subtitulo">LINKS: </strong>LINKS: Uma hiperligação, ou simplesmente uma ligação, é uma referência num documento hipertexto a outro documento ou a outro recurso. O seu significado é “atalho”, “caminho” ou “ligação”.
 
-							<br>LOGIN: É um conjunto de caracteres solicitado para os usuários que por algum motivo necessitam acessar algum sistema computacional. Geralmente os sistemas computacionais solicitam um login e uma senha para a liberação do acesso.
+							<br><strong class="subtitulo">LOGIN: </strong>É um conjunto de caracteres solicitado para os usuários que por algum motivo necessitam acessar algum sistema computacional. Geralmente os sistemas computacionais solicitam um login e uma senha para a liberação do acesso.
 
-							<br>PORTAL: É um site (sítio) na internet que funciona como centro aglomerador e distribuidor de tráfego para uma série de outros sites ou subsites dentro, e também fora, do domínio ou subdomínio da empresa gestora do portal.
+							<br><strong class="subtitulo">PORTAL: </strong>É um site (sítio) na internet que funciona como centro aglomerador e distribuidor de tráfego para uma série de outros sites ou subsites dentro, e também fora, do domínio ou subdomínio da empresa gestora do portal.
 
-							<br>SENHA: Uma senha ou palavra-chave é uma palavra ou ação secreta previamente convencionada entre duas partes como forma de reconhecimento. Em sistema de computação, senha são amplamente utilizadas para autenticar usuários e permitir-lhes o acesso
+							<br><strong class="subtitulo">SENHA: </strong>Uma senha ou palavra-chave é uma palavra ou ação secreta previamente convencionada entre duas partes como forma de reconhecimento. Em sistema de computação, senha são amplamente utilizadas para autenticar usuários e permitir-lhes o acesso
 							a informações personalizadas armazenadas no sistema.
 
 							</p>
@@ -113,6 +113,7 @@
 				</div>
 			</div>
 			</div>
+		</div>
 			<!-- Erro de Validação W3C Identificado -->
 			<div class="footer">
 				<?php include 'footer.php'; ?>
