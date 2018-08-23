98bf3e069ab3e1c9258ede82f5ad6627c8143cd4
tecnicowilliam <40693817+tecnicowilliam@users.noreply.github.com>
2018-08-23 08:21:42 -0300

    mudança de diretório consultas

--- listaUnidades.php
+++ listaUnidades.php
@@ -3,15 +3,6 @@
   //Conexão com Banco
   include 'conexao.inc.php';//inclusão do arquivo de conexão com o banco
   //include 'verificausuario.inc.php';
-
-
-         // Consultas
-  $filtro = isset($_GET['filtro'])?$_GET['filtro']:"";
-
-  $sql = "select * from unidades where nome like '%$filtro%' order by nome";
-  $consulta = mysqli_query($conexao, $sql);
-  $registros = mysqli_num_rows($consulta); //mostrar quantos registros existem no banco
-
    ?>
 
 <!DOCTYPE html>
@@ -61,34 +52,7 @@
     	  </table>
 
         <!-- Consultas -->
-         <?php
-
-           if (!empty($_GET['filtro'])) {
-
-  				 if ($registros > 0) {
-  					 echo "<br>Resultado da pesquisa com a palavra <strong> $filtro. </strong><br><br><br>";
-  					 echo "<strong>$registros registro(s) encontrado(s).</strong><br><br>"; //exibição da quanidade de registros encontrados
-  				 }else {
-  				 	echo "<br><br>Nenhum registro encontrado a palavra <strong> $filtro. </strong>";
-  				 }
-           ?> <a href="listaUnidades.php"><button type="submit" class="btn btn-default"> Limpar Pesquisa</button></a><br><br> <?php
-           while ($exibirRegistros =mysqli_fetch_array($consulta)) {
-             $codigo = $exibirRegistros[0];
-             $nome = $exibirRegistros[1];
-             $email = $exibirRegistros[2];
-             $profissao = $exibirRegistros[3];
-
-             echo "<article>";
-             echo "$codigo<br>";
-             echo "$nome<br>";
-             echo "$email<br>";
-             echo "$profissao<br><br>";
-             echo "</article>";
-           }
-        }
-           mysqli_close($conexao);
-            ?>
-
+        <?php include 'consultas/consUnidade.inc.php'; ?>
 
       </div>
     </div>
