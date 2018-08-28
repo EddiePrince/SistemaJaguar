3ae803c29250b6a686d3ffdfd10af244fc047d1f
luluane98 <40694004+luluane98@users.noreply.github.com>
2018-08-28 08:36:34 -0300

    Desenvolvedor.
    Coloquei o HTML dentro da Página do Desenvolvedor.

--- Desenvolvedor.php
+++ Desenvolvedor.php
@@ -13,12 +13,37 @@
 			<div class="container-fluid">
 
 				<div class="logo_admin">
-					<img src="img/logo.png" alt="Logo"> <a id="logout" href="logout.php" type="button" class="btn btn-default navbar-btn pull-right">  Logout  <img src="img/logout.png" alt="Logout"></a>
+					<img src="img/logo.png" alt="Logo">
 				</div>
 
 				<div class="container">
 					<div class="corpoAdm" id="corpoAdm">
-							<h2>Suporte</h2>
+						 <h1> Suporte</h1>
+
+      <h2>Desenvolvedores </h2>
+      Nome:Sistema de Contrle Integrado para Recebiemnto de Animais Silvestres no Naturatins (Sistema Juguar).
+      	<br>
+   <p> <h4> Logo: Sistema Juguar
+						<br>
+       
+  <h2> Contato(s) dos Desenvolvedores</h2>
+      William Alves Do Nascimento,<br>
+    Edwardes Junior Lima Gomes, Mayko Beckman Benício, Rômulo Miquéias da Silva Soares,Glenda Luciana Sousa da Silva.<br>
+    <br>
+	
+    <li>Telefone da Equipe</li>
+           <li>3218-4000</li>
+  	</p>
+      
+      <h2>Ferramentas Utilizadas</h2>
+      <br>Pelo(s) Desenvolvedor(es) são (MySQL), Workbench  Development Administration Database Design,.<br>
+     PHP Hypertext Preprocessor,<br>
+      JavaScript client-side, HTML HyperText Markup Language e CSS Cascading Style Sheets.</p>
+
+
+
+						
+							
 
 							<table class="table">
 
