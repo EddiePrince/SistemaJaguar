98b677e7461917fbee146c75a4af28b481066836
tecnicowilliam <williamtec.inf.int@gmail.com>
2018-08-29 11:43:33 -0300

    alteraçoes menu adm

--- menuAdm.php
+++ menuAdm.php
@@ -2,13 +2,10 @@
 <div>
 
 <nav class="menu">
-	<ul>
-		<li>
-			<li><a href="areaAdm.php">Home</a></li>
-			<li><a href="consultas.php">Consultas </a></li>
-			<li><a href="ajuda.php">Ajuda</a></li>
-		</li>
-	</ul>
+
+<a href="areaAdm.php"> <img class="home" src="img/home.png" alt="home"> Home</a>
+<a href="consultas.php"> <img class="consulta" src="img/consulta.png" alt="consultas"> Consultas </a>
+<a  src="img/help.png" href="ajuda.php" target="_blank"> precisa de ajuda <img class="icon-img" alt="Ajuda"></a>
 </nav>
 </div>
 		<!-- Fim Menu -->
