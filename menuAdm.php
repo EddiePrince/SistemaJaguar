aa6decab64498d3f77714352fe8182fbe2b80f6e
tecnicowilliam <williamtec.inf.int@gmail.com>
2018-08-29 19:13:59 -0300

    home

--- menuAdm.php
+++ menuAdm.php
@@ -1,11 +1,8 @@
       <!-- Menu -->
-<div>
+<div class="menu">
 
-<nav class="menu">
+	<a href="areaadm.php"><img class="home" src="img/home.png" alt="home"></a>
+	<a href="consultas.php"> <img class="consulta" src="img/consulta.png" alt="consultas"> </a>
 
-<a href="areaAdm.php"> <img class="home" src="img/home.png" alt="home"> Home</a>
-<a href="consultas.php"> <img class="consulta" src="img/consulta.png" alt="consultas"> Consultas </a>
-<a  src="img/help.png" href="ajuda.php" target="_blank"> precisa de ajuda <img class="icon-img" alt="Ajuda"></a>
-</nav>
 </div>
 		<!-- Fim Menu -->
