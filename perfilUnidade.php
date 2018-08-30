c99be21c753e94e28cd9f873e8a6e9bd12dc6567
EddiePrince <36989188+EddiePrince@users.noreply.github.com>
2018-08-30 09:06:42 -0300

    Bootstrap v3 removido

--- perfilUnidade.php
+++ perfilUnidade.php
@@ -10,7 +10,6 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css" href="style.css">
-		<link rel="stylesheet" type="text/css" href="style/bootstrapv3.min.css">
 		<link rel="stylesheet" href="style/bootstrap.min.css">
 		<title>Dados da unidade</title>
 	</head>
