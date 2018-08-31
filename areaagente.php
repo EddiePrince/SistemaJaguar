f7aa928d118fb2cb872fbc9f5bfe9e3ee6e48ca4
tecnicowilliam <williamtec.inf.int@gmail.com>
2018-08-31 10:08:17 -0300

    ajuste final

--- areaagente.php
+++ areaagente.php
@@ -34,13 +34,13 @@
     				<div class="container" id="container">
     				<div class="row">
     				<div class="col-md-4 mb-3">
-    				<a  href="listaUnidades.php" class="btn btn-default navbar-btn pull-right"> <img src="img/remove-user.png" alt="#"><br>  Unidade   </a>
+    				<a  href="listaUnidades.php" class="btn btn-default navbar-btn pull-right"> <img src="img/unidade.png" alt="#"><br>  Unidade   </a>
     				</div>
     				<div class="col-md-4 mb-3">
-    				<a href="listaUsuarios.php" class="btn btn-default navbar-btn pull-right"> <img src="img/remove-user.png" alt="#"><br> Usuário </a>
+    				<a href="listaUsuarios.php" class="btn btn-default navbar-btn pull-right"> <img src="img/usuario.png" alt="#"><br> Usuário </a>
     				</div>
     				<div class="col-md-4 mb-3">
-    				<a href="listaAnimais.php" class="btn btn-default navbar-btn pull-right"> <img src="img/remove-user.png" alt="#"><br>  Animal </a>
+    				<a href="listaAnimais.php" class="btn btn-default navbar-btn pull-right"> <img src="img/animal.png" alt="#"><br>  Animal </a>
 
     				</div>
     				</div>
@@ -52,6 +52,6 @@
 
     	<?php include 'footer.php';
     	?>
-    			
+
   </body>
 </html>
