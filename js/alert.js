c6e235e9ee3278829eab3badd5c6daea9a9296b4
tecnicowilliam <tecnicowilliam@gmail.com>
2018-09-17 11:49:43 -0300

    alerta ok
    Pelo menos no edge funciona ok

--- js/alert.js
+++ js/alert.js
@@ -2,6 +2,7 @@
 	var div = document.getElementById("alerta");
 	div.style.display="block";
 	var top = document.getElementById("boxtop");
+	div.style.animation = "gobottom 3s";
 	top.style.display="block";
 	div.style.top = "50%";
 	div.style.opacity = "1";
@@ -17,9 +18,10 @@
 
 function apagar(){
 	var div = document.getElementById("alerta");
-	div.style.animation = "gotop 3s";
-	div.style.top = "0";
+	 div.style.animation = "gotop 3s";
+	div.style.top = "0";	
 	div.style.opacity = "0";
+	div.style.remove();
 }
 
 
